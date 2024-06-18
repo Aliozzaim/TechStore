24ea715d4972f0685d3611cd9510ef35b0110978
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2024-06-18 22:42:09 +0200

    add: accordion comp

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from "@/components/ui/button"
+</script>
 
 <template>
-  <div>Home page</div>
+  <Button variant="secondary">Home page</Button>
 </template>
